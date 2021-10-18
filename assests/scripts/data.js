45276f3351db8eb0d71bfa30420bcfb732329812
imran <ikhalidf7@gmail.com>
2021-10-18 11:03:07 +0100

    react-app-uploaded

--- assests/scripts/data.js
+++ assests/scripts/data.js
@@ -3,8 +3,8 @@ export const projects = [
 		name: 'Ticket Tracker',
 		description:
 			'Using React JS to build this and SASS to style it, this project took 7 days to make',
-		link: '',
-		preview: '',
+		link: 'https://github.com/Imran-Khalid-code/Ticket-Tracker-ReactJS',
+		preview: 'https://ticket-tracker-f8ac9.web.app/',
 		imgsrc:
 			'https://images.unsplash.com/photo-1567748534085-467f8a8a475d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1227&q=80',
 		skills: ['React JS', 'SCSS'],
