35dc26834bc5d9cae9699d0a639fba1613fb5578
Imran Khalid <imran.khalid.code1@gmail.com>
2022-04-22 23:43:36 +0100

    project added

--- assests/scripts/data.js
+++ assests/scripts/data.js
@@ -1,6 +1,6 @@
 export const projects = [
 	{
-		name: 'Ticket Tracker',
+		name: 'Ticket Tracker-React',
 		description:
 			'Using React JS to build this and SASS to style it, this project took 7 days to make',
 		link: 'https://github.com/Imran-Khalid-code/Ticket-Tracker-ReactJS',
@@ -11,14 +11,23 @@ export const projects = [
 	},
 
 	{
-		name: 'Calculator',
+		name: 'BrewDog-API-React',
 		description:
-			'Using Vanilla JS to build this and CSS to style it, this project took 7 days to make',
-		link: 'https://github.com/Imran-Khalid-code/Calculator-JS',
-		preview: 'https://imran-khalid-code.github.io/Calculator-JS/',
+			'Using React JS to build this and SCSS to style it, this project took 14 days to make',
+		link: 'https://https://github.com/Imran-Khalid-code/BrewDog-Api-React-JS',
+		preview: 'https://brewdog-api-5ea7e.web.app/',
 		imgsrc:
-			'https://images.unsplash.com/photo-1594980596870-8aa52a78d8cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1325&q=80',
-		skills: ['Vanilla JS', 'CSS'],
+			'https://images.unsplash.com/photo-1617650493603-9d723f24a11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+		skills: [
+			'Vanilla JS',
+			'SCSS',
+			'React Hooks',
+			'Jest/React Testing',
+			'Routing',
+			'Api',
+			'map/filter',
+			'state',
+		],
 	},
 	{
 		name: 'Tic Tac Toe',
@@ -40,4 +49,4 @@ export const projects = [
 			'https://images.unsplash.com/photo-1579309401389-a2476dddf3d4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80',
 		skills: ['Vanilla JS', 'CSS'],
 	},
-]
+];
