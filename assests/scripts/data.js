d220c45c819e9c8196fa933eda93eb99c3ff02c0
Imran Khalid <imran.khalid.code1@gmail.com>
2022-04-25 14:26:07 +0100

    BrewDog-project-updated

--- assests/scripts/data.js
+++ assests/scripts/data.js
@@ -14,7 +14,7 @@ export const projects = [
 		name: 'BrewDog-API-React',
 		description:
 			'Using React JS to build this and SCSS to style it, this project took 14 days to make',
-		link: 'https://https://github.com/Imran-Khalid-code/BrewDog-Api-React-JS',
+		link: 'https://github.com/Imran-Khalid-code/BrewDog-Api-React-JS',
 		preview: 'https://brewdog-api-5ea7e.web.app/',
 		imgsrc:
 			'https://images.unsplash.com/photo-1617650493603-9d723f24a11a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
